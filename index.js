7d6691bb376ee9a047c6489b5900ae2c919719be
Derek Bautista <derek_roi_t_bautista@dlsu.edu.ph>
2024-08-13 01:42:33 +0800

    Added exercise 2 and 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchCity = exports.displayFilteredCity = exports.displayCity = exports.displayNames = exports.sayHello = void 0;
+exports.changeString = exports.isValidCode = exports.validateISBN = exports.searchCity = exports.displayFilteredCity = exports.displayCity = void 0;
 class Location {
     constructor(city, country, population) {
         this.city = city;
@@ -13,29 +13,6 @@ class Location {
         Population: ${this.population}\n\n`;
     }
 }
-var names = [];
-const sayHello = (inputName) => {
-    names.push(inputName);
-    (0, exports.displayNames)();
-};
-exports.sayHello = sayHello;
-const displayNames = () => {
-    let list = document.getElementById('myList');
-    // removes the content of list array
-    if (list) {
-        while (list.hasChildNodes() && list.firstChild) {
-            list.removeChild(list.firstChild);
-        }
-    }
-    names.forEach(element => {
-        let li = document.createElement('li');
-        li.innerText = element;
-        if (list) {
-            list.appendChild(li);
-        }
-    });
-};
-exports.displayNames = displayNames;
 let locations = [];
 const displayCity = (cityName, countryName, population) => {
     locations.push(new Location(cityName, countryName, population));
@@ -75,10 +52,75 @@ exports.displayFilteredCity = displayFilteredCity;
 const searchCity = (keyword) => {
     let filteredLocations = [];
     locations.forEach(loc => {
-        if (loc.city.includes(keyword) || loc.country.includes(keyword)) {
+        if (loc.city.toLowerCase().includes(keyword) || loc.country.toLowerCase().includes(keyword)) {
             filteredLocations.push(loc);
         }
     });
     (0, exports.displayFilteredCity)(filteredLocations);
 };
 exports.searchCity = searchCity;
+const validateISBN = (code) => {
+    console.log(`${code} -> ${(0, exports.isValidCode)(code)}`);
+};
+exports.validateISBN = validateISBN;
+const isValidCode = (code) => {
+    let i = 1;
+    let isbnValue = 0;
+    let cValue = 0;
+    for (let c of code) {
+        if (c === 'X') {
+            if (i != 10)
+                return false;
+            cValue = 10;
+        }
+        else {
+            cValue = +c;
+        }
+        isbnValue += cValue * i;
+        i++;
+    }
+    if (isbnValue % 11 != 0)
+        return false;
+    return true;
+};
+exports.isValidCode = isValidCode;
+const changeString = (word) => {
+    word = word.toLowerCase();
+    console.log(word);
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+        let ascii = word.charCodeAt(i);
+        if (ascii === 122) {
+            newWord += String.fromCharCode(65); // convert z to A
+        }
+        else if (ascii >= 97 && ascii <= 121) {
+            let newChar = String.fromCharCode(ascii + 1); //move to the next char
+            if (isVowel(newChar)) {
+                newWord += newChar.toUpperCase(); //uppercase the char if it is a vowel
+            }
+            else {
+                newWord += newChar; //concat consonant
+            }
+        }
+        else {
+            newWord += word.charAt(i);
+        }
+    }
+    console.log(newWord);
+    displayStringResult(newWord);
+};
+exports.changeString = changeString;
+function displayStringResult(resultText) {
+    let resultBox = document.getElementById('string-result');
+    let result = document.createElement('div');
+    result.textContent = resultText;
+    // removes the content of list array
+    if (resultBox) {
+        resultBox.appendChild(result);
+    }
+}
+function isVowel(c) {
+    if (c === "a" || c === "e" || c === "i" || c === "o" || c === "u")
+        return true;
+    return false;
+}
--- index.ts
+++ index.ts
@@ -17,32 +17,6 @@ class Location{
 
 }
 
-var names:string[] = []
-
-
-export const sayHello = (inputName: string) => {
-    names.push(inputName)
-    displayNames();
-}
-
-export const displayNames = () => {
-    let list = document.getElementById('myList')
-
-    // removes the content of list array
-    if(list){
-        while(list.hasChildNodes() && list.firstChild){
-            list.removeChild(list.firstChild);
-        }
-    }
-    names.forEach(element => {
-        let li = document.createElement('li')
-        li.innerText = element
-        if(list){
-            list.appendChild(li);
-        }
-    })
-}
-
 let locations:Location[] = []
 
 export const displayCity = (cityName:string, countryName:string, population:number) => {
@@ -89,10 +63,82 @@ export const searchCity = (keyword:string) => {
     let filteredLocations:Location[] = []
 
     locations.forEach(loc => {
-       if(loc.city.includes(keyword) || loc.country.includes(keyword)){
+       if(loc.city.toLowerCase().includes(keyword) || loc.country.toLowerCase().includes(keyword)){
         filteredLocations.push(loc)
        }
     })
 
     displayFilteredCity(filteredLocations)
+}
+
+export const validateISBN = (code:string) => {
+    console.log(`${code} -> ${isValidCode(code)}`)
+}
+
+export const isValidCode = (code:string) =>{
+    let i = 1
+    let isbnValue = 0
+    let cValue = 0
+
+    for(let c of code){
+        if(c === 'X'){
+            if(i != 10)
+                return false;
+            cValue = 10
+        }
+        else{
+            cValue = +c
+        }
+        isbnValue += cValue*i
+        i++
+    }
+
+    if(isbnValue%11 != 0)
+        return false
+    return true
+}
+
+export const changeString = (word:String) => {
+    word = word.toLowerCase()
+    console.log(word)
+    let newWord:string = "";
+    for(let i = 0; i < word.length; i++){
+        let ascii = word.charCodeAt(i)
+
+        if(ascii === 122){
+            newWord += String.fromCharCode(65) // convert z to A
+        }
+        else if (ascii >= 97 && ascii <= 121){
+            let newChar = String.fromCharCode(ascii+1) //move to the next char
+            if(isVowel(newChar)){
+                newWord += newChar.toUpperCase()//uppercase the char if it is a vowel
+            }
+            else{
+                newWord += newChar//concat consonant
+            }
+        }
+        else{
+            newWord += word.charAt(i)
+        }
+    }
+
+    console.log(newWord)
+    displayStringResult(newWord)
+}
+
+
+function displayStringResult (resultText:string){
+    let resultBox = document.getElementById('string-result')
+    let result = document.createElement('div')
+    result.textContent = resultText
+    // removes the content of list array
+    if(resultBox){
+        resultBox.appendChild(result);
+    }
+}
+
+function isVowel (c:string){
+    if(c === "a" || c === "e" || c ==="i" || c ==="o" || c === "u")
+        return true
+    return false
 }
